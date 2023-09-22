df38e310af2c58d9ad1b596e6094f931881065a0
Sophon96 <71684640+Sophon96@users.noreply.github.com>
2023-09-21 18:22:03 -0700

    wip: in case shit hits the fan
    Signed-off-by: Sophon96 <71684640+Sophon96@users.noreply.github.com>

--- app/routes/admin.clubs.add/route.tsx
+++ app/routes/admin.clubs.add/route.tsx
@@ -1,6 +1,96 @@
+import * as z from "zod";
+import AutoForm, { AutoFormSubmit } from "~/components/ui/auto-form";
+import { useSubmit } from "@remix-run/react";
+import { ActionArgs } from "@remix-run/cloudflare";
+
+const formSchema = z.object({
+  clubName: z.string({ required_error: "This field must be filled." }),
+  description: z.string({ required_error: "This field must be filled." }),
+  advisor: z
+    .string({ required_error: "This field must be filled." })
+    .email("Please enter a valid email."),
+  officers: z.array(
+    z.object({
+      email: z
+        .string({ required_error: "This field must be filled." })
+        .email("Please enter a valid email."),
+      role: z.string().min(1, { message: "This field must be filled." }),
+    })
+  ),
+  meetings: z.array(
+    z.object({
+      location: z.string({ required_error: "This field must be filled." }),
+      interval: z.coerce
+        .number({ required_error: "This field must be filled." })
+        .int("Please enter a valid integer.")
+        .min(1, { message: "Please enter a positive number." }),
+      frequency: z.enum(["daily", "weekly", "monthly"]),
+      startingDate: z.coerce
+        .date({
+          required_error: "This field must be filled.",
+        })
+        .transform((date) => date.getTime()),
+    })
+  ),
+  approved: z.boolean().describe("Approved for this school year"),
+});
+
+export async function action({ request, params }: ActionArgs) {
+  let form = await request.json<z.infer<typeof formSchema>>();
+
+
+  // TODO: DEBUG
+  console.log(JSON.stringify(form))
+  return null;
+}
+
 export default function AdminAddClub() {
-  return <>
-    <h1>Placeholder</h1>
-    <h3>Admin page for adding a club</h3>
-  </>
+  const submit = useSubmit();
+  /*const formSchema = z.object({
+    clubName: z.string({ required_error: "This field must be filled." }),
+    description: z.string({ required_error: "This field must be filled." }),
+    advisor: z
+      .string({ required_error: "This field must be filled." })
+      .email("Please enter a valid email."),
+    officers: z.array(
+      z.object({
+        email: z
+          .string({ required_error: "This field must be filled." })
+          .email("Please enter a valid email."),
+        role: z.string().min(1, { message: "This field must be filled." }),
+      })
+    ),
+    meetings: z.array(
+      z.object({
+        location: z.string({ required_error: "This field must be filled." }),
+        interval: z.coerce
+          .number({ required_error: "This field must be filled." })
+          .int("Please enter a valid integer.")
+          .min(1, { message: "Please enter a positive number." }),
+        frequency: z.enum(["daily", "weekly", "monthly"]),
+        startingDate: z.coerce
+          .date({
+            required_error: "This field must be filled.",
+          })
+          .transform((date) => date.getTime()),
+      })
+    ),
+    approved: z.boolean().describe("Approved for this school year"),
+  });*/
+  return (
+    <>
+      <div className="w-full max-w-7xl m-auto space-y-4 py-4">
+        <h2 className="text-3xl font-bold tracking-tight">Add a club</h2>
+        <hr />
+        <AutoForm
+          formSchema={formSchema}
+          onSubmit={(data) =>
+            submit(data, { method: "POST", encType: "application/json" })
+          }
+        >
+          <AutoFormSubmit>Submit</AutoFormSubmit>
+        </AutoForm>
+      </div>
+    </>
+  );
 }
--- app/routes/clubs._index/route.tsx
+++ app/routes/clubs._index/route.tsx
@@ -1,4 +1,5 @@
 import { useParams } from "@remix-run/react";
+import {Card, CardDescription, CardHeader, CardTitle} from "~/components/ui/card";
 
 export default function Club() {
   const params = useParams();
@@ -6,9 +7,20 @@ export default function Club() {
 
   return (
     <>
-      <h1 className="">Placeholder</h1>
-      <h3>Information about club {id}</h3>
-        <div className="w-max grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 m-auto">
+      {/*<h1 className="">Placeholder</h1>*/}
+      {/*<h3>Information about club {id}</h3>*/}
+        <div className="flex flex-wrap justify-center items-center gap-2 max-w-7xl m-auto">
+          <Card className="w-96 overflow-hidden">
+            <img src="https://images.unsplash.com/photo-1682687221175-fd40bbafe6ca?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=300&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY5NTIxOTM4Mw&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=600" alt=""/>
+            <CardHeader>
+              <CardTitle>
+                Title
+              </CardTitle>
+              <CardDescription>
+                Jinzo faiya faiba waipa taiga taiga t-t-t-t-taiga chape ape kara kina chape ape kara kina myohontuske clap waipa
+              </CardDescription>
+            </CardHeader>
+          </Card>
           <div className="bg-blue-600 w-96 h-48 rounded">01</div>
           <div className="bg-blue-600 w-96 h-48 rounded">02</div>
           <div className="bg-blue-600 w-96 h-48 rounded">03</div>
