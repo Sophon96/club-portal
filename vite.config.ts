0f6428fe3d5f9a9061ed0e8b98b13c23b7a57c9f
Sophon96 <71684640+Sophon96@users.noreply.github.com>
2024-08-30 13:00:59 -0700

    chore(netlify): switch from edge to serverless functions because prisma

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { netlifyPlugin } from "@netlify/remix-edge-adapter/plugin";
+import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
 import { installGlobals } from "@remix-run/node";
 
 installGlobals();
