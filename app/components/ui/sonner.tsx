24dd297dbd2e9d06b98f1b99af969278a8943b38
Sophon96 <71684640+Sophon96@users.noreply.github.com>
2024-09-02 16:19:05 -0700

    feat: switch to better-looking loading icon for sonner

--- app/components/ui/sonner.tsx
+++ app/components/ui/sonner.tsx
@@ -2,7 +2,7 @@ import {
   AlertTriangle,
   CheckCircle,
   Info,
-  Loader,
+  Loader2,
   XCircle,
 } from "lucide-react";
 import { useTheme } from "remix-themes";
@@ -33,7 +33,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
         info: <Info className="h-4 w-4 text-blue-500" />,
         warning: <AlertTriangle className="h-4 w-4 text-amber-500" />,
         error: <XCircle className="h-4 w-4 text-red-500" />,
-        loading: <Loader className="h-4 w-4 text-gray-500 animate-spin" />,
+        loading: <Loader2 className="h-4 w-4 text-gray-500 animate-spin" />,
       }}
       {...props}
     />
